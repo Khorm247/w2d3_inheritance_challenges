bd745e8a2ec06ff9ad59a3c2d8b931eaf7fd5cf2
Reebal Sami <reebal.sami@gmail.com>
2024-01-31 11:09:05 +0100

    step 2 Car Class added

---
+++ src/main/java/de/neuefische/Car.java
@@ -0,0 +1,29 @@
+package de.neuefische;
+
+public class Car extends Vehicle{
+
+    private int numOfDoors;
+
+
+    public Car(String model, String manufacturer, int yearOfManufacture, double price, int numOfDoors) {
+
+        super(model, manufacturer, yearOfManufacture, price);
+        this.numOfDoors = numOfDoors;
+    }
+
+    public int getNumOfDoors() {
+        return numOfDoors;
+    }
+
+    public void setNumOfDoors(int numOfDoors) {
+        this.numOfDoors = numOfDoors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "numOfDoors=" + numOfDoors +
+                '}';
+    }
+}
+
